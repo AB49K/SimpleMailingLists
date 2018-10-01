9af1539733353e2e76b1918b3283581d6cf5edd5
Andrew <andrew@ab49k.net>
2018-10-01 22:23:40 +1000

    Added confirmation email (unfinished), added TODO to readme

--- main.py
+++ main.py
@@ -63,6 +63,25 @@ def SendEmail(receiver, message, section_name, config):
         print(e)
 
 def MonitorMail(section_name, config):
+    helpmessage="""
+Valid commands
+help - This message
+Subscribe <list> subscribes to a mailing list
+Unsubscribe <list> Will unsubscribe you from a mailing list
+"""
+    subscribe_message="""
+Your have asked to be subscribed to {maillist}
+{email}
+If you did not request this, you may disregard this message.
+To confirm subscription, please reply to this message and leave the subject the same.
+
+Thanks.
+"""
+    subscription_confirm="""
+You have subscribed to {maillist}
+
+To unsubscribe send an email to {email} with the subject "unsubscribe"
+"""
     try:
          with IMAPClient(host=config.get(section_name, "imap_server")) as client:
             client.login(config.get(section_name, "imap_username"), config.get(section_name, "imap_password"))
@@ -73,13 +92,7 @@ def MonitorMail(section_name, config):
                 print(email_message.get('From'), email_message.get('Subject'))
                 if section_name=="ADMIN":
                     msg=MIMEMultipart()
-                    helpmessage="""
-                    Valid commands
-                        help - This message
-                        Subscribe <list> subscribes to a mailing list
-                        Unsubscribe <list> Will unsubscribe you from a mailing list
-                        """
-                    #msg['From']=config.get(section_name, "email_address")
+                    msg['From']=config.get(section_name, "email_address")
                     msg['To']=email_message.get('From')
                     msg['Subject']="Help Message"
                     msg.attach(MIMEText(helpmessage, 'plain'))
@@ -87,36 +100,39 @@ def MonitorMail(section_name, config):
                     if 'help' in email_message.get('Subject').lower():
                         SendEmail(email_message.get('From'), msg.as_string(), "ADMIN", config)
                         client.move(uid, "LIST_ARCHIVE")
+                    if "subscribe" in email_message.get("Subject").lower():
+                        for section_name in config.sections():
+                            if section_name.lower() in email_message.get("Subject").lower():
+                                msg=MIMEMultipart()
+                                msg['From']=config.get(section_name, "email_address")
+                                msg['To']=email_message.get('From')
+                                subscription=email_message.get("Subject").lower()
+                                subscription=subscription.replace(" ", "")
+                                subscription=subscription.split("subscribe")
+                                subscription=subscription[1]
+                                msg['Subject']="Subscribe {}".format(subscription)
+                                mailbody=subscribe_message.format(maillist=subscription, email=config.get(subscription.capitalize(), "email_address"))
+                                print(mailbody)
+                                msg.attach(MIMEText(mailbody, 'plain'))
+
+                                SendEmail(email_message.get('From'), msg.as_string(), "ADMIN", config)
+
+
+
+
                 else:
                     #here is where we decide if it's an command, or a message to pass along.
-                    pass
+                    if "subscribe" in email_message.get("Subject").lower():
+                        msg=MIMEMultipart()
+                        msg['From']=config.get(section_name, "email_address")
+                        msg['To']=email_message.get('From')
+                        msg['Subject']="Please send the subscribe request to {}", config.get("ADMIN", "email_address")
+                        SendEmail(email_message.get('From'), msg.as_string(), section_name, config)
+
 
     except Exception as e:
-        print("An error occured\n")
+        print("An error occured in the mailmonitor function\n")
         print(e)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 config=LoadConfig()
--- readme.md
+++ readme.md
@@ -3,4 +3,10 @@
 
 This was built in response to being unable to find a super simple mailing list software that doesn't want aliases with postfix or any number of unessasary system integrations.
 
+TODO:
+database system to keep user information.
+confirmation of subscription.
+email filtering to make sure only subscribed members can email the list
+
+finding the most efficient way to mass mail (how many bcc address can I add to an email?
 
