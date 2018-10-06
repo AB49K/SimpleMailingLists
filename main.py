59f1e6a64b11af1d8b656cd709e966e77e338c00
Andrew <andrew@ab49k.net>
2018-10-07 02:20:13 +1000

    Added database check/creation at script startup. boilerplate database functions. not working yet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+import os.path
 import sys
 from imapclient import IMAPClient
 import email
@@ -63,24 +64,12 @@ def SendEmail(receiver, message, section_name, config):
         print(e)
 
 def MonitorMail(section_name, config):
+    #These are hardcoded for a reason.
     helpmessage="""
 Valid commands
 help - This message
 Subscribe <list> subscribes to a mailing list
 Unsubscribe <list> Will unsubscribe you from a mailing list
-"""
-    subscribe_message="""
-Your have asked to be subscribed to {maillist}
-{email}
-If you did not request this, you may disregard this message.
-To confirm subscription, please reply to this message and leave the subject the same.
-
-Thanks.
-"""
-    subscription_confirm="""
-You have subscribed to {maillist}
-
-To unsubscribe send an email to {email} with the subject "unsubscribe"
 """
     try:
          with IMAPClient(host=config.get(section_name, "imap_server")) as client:
@@ -100,23 +89,9 @@ To unsubscribe send an email to {email} with the subject "unsubscribe"
                     if 'help' in email_message.get('Subject').lower():
                         SendEmail(email_message.get('From'), msg.as_string(), "ADMIN", config)
                         client.move(uid, "LIST_ARCHIVE")
+                    #I probably want to move this bit to it's own function. maybe later (never)
                     if "subscribe" in email_message.get("Subject").lower():
-                        for section_name in config.sections():
-                            if section_name.lower() in email_message.get("Subject").lower():
-                                msg=MIMEMultipart()
-                                msg['From']=config.get(section_name, "email_address")
-                                msg['To']=email_message.get('From')
-                                subscription=email_message.get("Subject").lower()
-                                subscription=subscription.replace(" ", "")
-                                subscription=subscription.split("subscribe")
-                                subscription=subscription[1]
-                                msg['Subject']="Subscribe {}".format(subscription)
-                                mailbody=subscribe_message.format(maillist=subscription, email=config.get(subscription.capitalize(), "email_address"))
-                                print(mailbody)
-                                msg.attach(MIMEText(mailbody, 'plain'))
-
-                                SendEmail(email_message.get('From'), msg.as_string(), "ADMIN", config)
-
+                        Subscribe(email_message, section_name, config)
 
 
 
@@ -129,14 +104,92 @@ To unsubscribe send an email to {email} with the subject "unsubscribe"
                         msg['Subject']="Please send the subscribe request to {}", config.get("ADMIN", "email_address")
                         SendEmail(email_message.get('From'), msg.as_string(), section_name, config)
 
+                    else:
+                        SendToList(email_message, section_name, config)
 
     except Exception as e:
         print("An error occured in the mailmonitor function\n")
         print(e)
 
+def GenerateConfirmationString():
+    #This feature is here to stop someone being able to spoof an email and get someone else on the list - we have to implement this
+    #because we are can't trust the mail server. a lot of mail servers don't stop spoofing.
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
+
+def CreateDatabase():
+    print("Checking if mailer.db exists.")
+    if os.path.isfile('mailer.db'):
+        print("mailer.db exists. There is no option to re-write it here. please move or delete it if you want to rebuild it\n")
+        return 0
+    try:
+        print("mailer.db does not exist. creating now")
+        conn=sqlite3.connect('mailer.db')
+        c=conn.cursor()
+        c.execute("""
+            CREATE TABLE mailer(email_address text, mailing_list text, confirmation_string text, subscribed int)""")
+        conn.commit()
+        conn.close()
+        print("Created new database mailer.db")
+    except Exception as e:
+        print("An Error occured in the CreatDatabase function")
+        print(e)
+
+
+def MailSQL(command):
+    conn=sqlite3.connect('mailer.db')
+    c=conn.cursor()
+    c.execute(command)
+    return c.fetchone()
+
+def CheckIfSubscribed(email_message, section_name):
+    #placeholder function
+    m=MailSQL(("select * from mailer where email_address={} and mailing_list={}").format(email_message.get('From'), section_name.lower))
+    print(m)
+
+
+def SendToList(email_message,section_name,config):
+    pass
+
+def Subscribe(email_message,section_name,config):
+    #These are hardcoded for a reason.
+    helpmessage="""
+Valid commands
+help - This message
+Subscribe <list> subscribes to a mailing list
+Unsubscribe <list> Will unsubscribe you from a mailing list
+"""
+    subscribe_message="""
+Your have asked to be subscribed to {maillist}
+{email}
+If you did not request this, you may disregard this message.
+To confirm subscription, please reply to this message and leave the subject the same.
+
+Thanks.
+"""
+    subscription_confirm="""
+You have subscribed to {maillist}
+
+To unsubscribe send an email to {email} with the subject "unsubscribe"
+"""
+    for section_name in config.sections():
+        if section_name.lower() in email_message.get("Subject").lower():
+            CheckIfSubscribed(email_message, section_name)
+            msg=MIMEMultipart()
+            msg['From']=config.get(section_name, "email_address")
+            msg['To']=email_message.get('From')
+            subcription=email_message.get("Subject").lower()
+            subscription=subscription.replace(" ", "")
+            subscription=subscription.split("subscribe")
+            subscription=subscription[1]
+            msg['Subject']="Subscribe {} - {}".format(subscription, GenerateConfirmationString())
+            mailbody=subscribe_message.format(maillist=subscription, email=config.get(subscription.capitalize(), "email_address"))
+            print(mailbody)
+            msg.attach(MIMEText(mailbody, 'plain'))
+            SendEmail(email_message.get('From'), msg.as_string(), "ADMIN", config)
 
 config=LoadConfig()
 TestConfig(config)
+CreateDatabase()
 for section_name in config.sections():
 
     MonitorMail(section_name, config)
